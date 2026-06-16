8e5f1b7772399429e3ac141a20941be5bb4f5b28
am-tazfahouth <dv.bugslayer@gmail.com>
2026-06-16 23:57:26 +0300

    feat: wire weather forecast into app entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sky_line/core/config/app_theme.dart';
 import 'package:sky_line/core/enums/setting_theme.dart';
 import 'package:sky_line/core/utils/platform_utils.dart';
+import 'package:sky_line/features/weather_forecast/data/repositories/weather_repository_impl.dart';
+import 'package:sky_line/features/weather_forecast/data/sources/weather_remote_source.dart';
+import 'package:sky_line/features/weather_forecast/domain/usecases/fetch_weather_usecase.dart';
+import 'package:sky_line/features/weather_forecast/presentation/blocs/weather_forecast_bloc.dart';
+import 'package:sky_line/features/weather_forecast/presentation/screens/weather_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,13 +24,22 @@ class MyApp extends StatelessWidget {
     return AnnotatedRegion(
       value: PlatformUtils.getSystemUiStyle(SettingTheme.system, context),
       child: MaterialApp(
-        title: 'SkiLine',
-        home: Scaffold(),
+        title: 'SkyLine',
+        home: BlocProvider(
+          create: (_) => WeatherForecastBloc(
+            FetchWeatherUseCase(
+              WeatherRepositoryImpl(
+                WeatherRemoteSource(Dio()),
+              ),
+            ),
+          ),
+          child: const WeatherScreen(),
+        ),
         theme: appTheme.light(),
         darkTheme: appTheme.dark(),
         debugShowCheckedModeBanner: false,
         themeMode: SettingTheme.getThemeMode(SettingTheme.system),
-      ), 
+      ),
     );
   }
 }
