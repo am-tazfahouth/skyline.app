84b98eadefa41e4d708713c38f5df8d7a51de227
am-tazfahouth <dv.bugslayer@gmail.com>
2026-06-18 16:04:03 +0300

    fix: expose WeatherRepository interface in DI, remove dead usecase

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,12 +2,13 @@ import 'package:dio/dio.dart';
 import 'package:sky_line/core/config/db_helper/db_helper.dart';
 import 'package:sky_line/features/weather_forecast/data/repositories/weather_repository_impl.dart';
 import 'package:sky_line/features/weather_forecast/data/sources/weather_remote_source.dart';
+import 'package:sky_line/features/weather_forecast/domain/repositories/weather_repository.dart';
 
 class InjectionContainer {
   static late final Dio dio;
   static late final DbHelper dbHelper;
   static late final WeatherRemoteSource weatherRemoteSource;
-  static late final WeatherRepositoryImpl weatherRepository;
+  static late final WeatherRepository weatherRepository;
 
   static Future<void> init() async {
     dbHelper = await DbHelper.init();
